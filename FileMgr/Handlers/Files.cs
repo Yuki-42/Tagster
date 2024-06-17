c6830b11eaf5b64bd017b6bf9295561313d9b14a
Yuki-42 <kylepannan@gmail.com>
2024-06-17 13:03:10 +1000

    Started Files.cs

--- FileMgr/Handlers/Files.cs
+++ FileMgr/Handlers/Files.cs
@@ -1,4 +1,7 @@
 using System.Data.SQLite;
+using FileMgr.Objects;
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedMember.Global
 
 namespace FileMgr.Handlers;
 
@@ -27,4 +30,53 @@ public class Files
         _connection = connection;
         _config = config;
     }
+
+    /// <summary>
+    /// Gets a file by its ID.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public Objects.File? Get(long id)
+    {
+        // Create a new command.
+        SQLiteCommand command = new("SELECT * FROM files WHERE id = @id;", _connection);
+
+        // Add the parameter.
+        command.Parameters.AddWithValue("@id", id);
+
+        // Execute the command and get the reader.
+        SQLiteDataReader reader = command.ExecuteReader();
+
+        if (!reader.HasRows)
+        {
+            reader.Close();
+            return null;
+        }
+
+        // Read the data.
+        reader.Read();
+        long fileId = reader.GetInt64(0);
+        DateTime added = reader.GetDateTime(1);
+        string filePath = reader.GetString(2);
+        reader.Close();
+
+        // Create a new file object and return it
+        return new Objects.File(fileId, added, filePath, GetTags(fileId));
+    }
+
+
+
+    public Objects.File Add(FileInfo file)
+    {
+
+    }
+
+    public Objects.File Add(
+        FileInfo file,
+        List<Tag> tags
+        )
+    {
+
+    }
+
 }
