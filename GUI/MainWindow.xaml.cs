ea7ce91230bcf735cb2ddc5787957d7098b337a5
Yuki <kylepannan@gmail.com>
2024-06-28 00:00:15 +1000

    Added base for MainWindow

--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -1,23 +1,49 @@
-﻿using System.Text;
+﻿using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using FileMgr;
+
+// Project-specific
 
 namespace GUI;
 
 /// <summary>
-/// Interaction logic for MainWindow.xaml
+///     Interaction logic for MainWindow.xaml
 /// </summary>
 public partial class MainWindow : Window
 {
+    /// <summary>
+    ///     Shared top menu code.
+    /// </summary>
+    private readonly TopMenu _topMenu;
+
+    /// <summary>
+    ///     File Managers. Key is the directory.
+    /// </summary>
+    // ReSharper disable once PrivateFieldCanBeConvertedToLocalVariable
+    // ReSharper disable once FieldCanBeMadeReadOnly.Local
+    private List<KeyValuePair<DirectoryInfo, FileManager>> _fileManagers;
+
     public MainWindow()
     {
         InitializeComponent();
+        _fileManagers = new List<KeyValuePair<DirectoryInfo, FileManager>>();
+        _topMenu = new TopMenu(ref _fileManagers);
+    }
+
+    // Expose TopMenu events
+    private void TopMenu_File_Open_Click(object sender, RoutedEventArgs e)
+    {
+        _topMenu.TopMenu_File_Open_Click(sender, e);
+    }
+
+    private void TopMenu_File_Manage_Click(object sender, RoutedEventArgs e)
+    {
+        _topMenu.TopMenu_File_Manage_Click(sender, e);
+    }
+
+    private void TopMenu_File_Exit_Click(object sender, RoutedEventArgs e)
+    {
+        _topMenu.TopMenu_File_Exit_Click(sender, e);
+        Close();
     }
 }
