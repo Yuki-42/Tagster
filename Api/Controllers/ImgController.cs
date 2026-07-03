67c3d14f0594b60c9493fef65b2ff6122d9eff55
Yuki-42 <kylepannan@gmail.com>
2026-07-03 20:19:32 +1000

    Created image get endpoints

---
+++ Api/Controllers/ImgController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+/// <summary>
+/// Endpoint used for retrieving images and correctly adding required metadata.
+/// </summary>
+public class ImgController : ControllerBase
+{
+	/// <summary>
+	/// Gets an original quality image in an embeddable format.
+	/// </summary>
+	/// <param name="id">Media ID.</param>
+	/// <returns>Media.</returns>
+	[HttpGet("/original/{id:guid}")]
+	public async Task<ActionResult> GetOriginal(Guid id)
+	{
+		throw new NotImplementedException();
+	}
+
+	/// <summary>
+	/// Gets a thumbnail quality image if exists in an embeddable format.
+	/// </summary>
+	/// <param name="id">Media ID.</param>
+	/// <returns>Media.</returns>
+	[HttpGet("/thumbnail/{id:guid}")]
+	public async Task<ActionResult> GetThumbnail(Guid id)
+	{
+		throw new NotImplementedException();
+	}
+
+}
